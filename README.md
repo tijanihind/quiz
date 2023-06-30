fc411bed1d59cf49e9afc3b12674461aa1cd6a0f
tijanihind <tijani.hind200@gmail.com>
2023-06-29 23:40:06 -0400

    edited quiz and added more questions and css

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# quiz
+# JavaScript Quiz Application
+
+This repository contains a JavaScript quiz application that allows users to answer multiple-choice questions, tracks their score, and displays the remaining time. It includes various functions to manage the quiz, such as starting the quiz, displaying questions, checking answers, handling the timer, ending the quiz, and saving/displaying the highest score.
+
+## Code Annotations
+
+Here are some comments regarding the code:
+
+- The code utilizes `addEventListener` to bind click events to the start button and submit event to the score form.
+- The `loadHighestScore` function retrieves the highest score from local storage or returns `null` if not found.
+- The `saveHighestScore` function saves the highest score to local storage.
+- The `displayHighestScore` function displays the highest score on the page.
+- The `startQuiz` function resets the quiz, hides the start button, displays the quiz container, starts the timer, and shows the first question.
+- The `resetQuiz` function resets the quiz variables, hides the game over container, displays the quiz container, clears the question and choices, sets the timer to default, and resets the initials input value.
+- The `startTimer` function sets an interval to decrement the time remaining by 1 second until it reaches 0, updating the timer container. If the time runs out, it stops the timer and ends the quiz.
+- The `showQuestion` function dynamically displays the current question and its choices by creating buttons for each choice and appending them to the choices container.
+- The `checkAnswer` function compares the selected answer with the correct answer for the current question, updates the score or deducts time accordingly, and advances to the next question or ends the quiz if there are no more questions or time runs out.
+- The `endQuiz` function stops the timer, hides the quiz container, shows the game over container, sets the timer to 0, and checks if the score is higher than the previous highest score. If so, it prompts a message and saves the new highest score.
+- The `saveScore` function handles the submission of the user's initials for saving the score. It prevents the default form submission, retrieves the initials, saves the score and initials, displays the highest score, resets the quiz, and shows the start button.
+
+The quiz can be accessed [here](https://tijanihind.github.io/quiz/).
+  
+**Note:** The code assumes the presence of HTML elements with specific IDs (`quiz-container`, `question-container`, `choices-container`, `start-btn`, `timer-container`, `game-over-container`, `score-form`, `initials`, `highest-score-container`) that are not included in the provided code snippet.
+
+The `displayHighestScore(highestScore)` function is called at the end to display the highest score when the page loads.
+
+The code also includes a `console.log(highestScore)` statement, which logs the highest score object to the console.
+
+## Screenshots
+
+![Screenshot 1](/Image20230629232104.png)
+![Screenshot 2](/Image20230629232117.png)
+![Screenshot 2](/Image20230629232121.png)
--- script.js
+++ script.js
@@ -1,78 +1,173 @@
-// Define your quiz questions and answers as an array of objects
-const quizQuestions = [
-    {
-      question: "Question 1",
-      choices: ["Answer 1", "Answer 2", "Answer 3"],
-      correctAnswer: 0
-    },
-    {
-      question: "Question 2",
-      choices: ["Answer 1", "Answer 2", "Answer 3"],
-      correctAnswer: 1
-    },
-    // Add more questions here
-  ];
-  
-  const quizContainer = document.getElementById("quiz-container");
-  const questionContainer = document.getElementById("question-container");
-  const choicesContainer = document.getElementById("choices-container");
-  const startButton = document.getElementById("start-btn");
-  const timerContainer = document.getElementById("timer-container");
-  const gameOverContainer = document.getElementById("game-over-container");
-  const scoreForm = document.getElementById("score-form");
-  const initialsInput = document.getElementById("initials");
-  
-  let currentQuestionIndex = 0;
-  let timer;
-  let timeRemaining = 20; // Adjust as needed
-  let score = 0;
-  
-  startButton.addEventListener("click", startQuiz);
-  scoreForm.addEventListener("submit", saveScore);
-  
-  function startQuiz() {
-    startButton.style.display = "none";
-    quizContainer.style.display = "block";
+const quizQuestions = [{
+    question: "What is the correct JavaScript syntax to change the content of the following HTML element? <p id='demo'>This is a demonstration.</p>",
+    choices: ["document.getElement('p').innerHTML = 'Hello World!'';", "document.getElementByName('p').innerHTML = 'Hello World!';", "document.getElementById('demo').innerHTML = 'Hello World!';", "#demo.innerHTML = 'Hello World!';"],
+    correctAnswer: 2
+},
+{
+    question: "The external JavaScript file must contain the <script> tag.",
+    choices: ["False", "True"],
+    correctAnswer: 0
+},
+{
+    question: "How do you create a function in JavaScript?",
+    choices: ["function = myFunction()", "function:myFunction()", "function myFunction()"],
+    correctAnswer: 2
+},
+{
+    question: "How do you call a function named 'myFunction'?",
+    choices: ["call function myFunction()", "myFunction()", "call myFunction()"],
+    correctAnswer: 1
+},
+{
+    question: "How to write an IF statement in JavaScript?",
+    choices: ["if i = 5 then", "if i == 5 then", "if (i == 5)", "if i = 5"],
+    correctAnswer: 2
+},
+{
+    question: "How does a FOR loop start?",
+    choices: ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for (i <= 5; i++)", "for i = 1 to 5"],
+    correctAnswer: 1
+},
+];
+
+const quizContainer = document.getElementById("quiz-container");
+const questionContainer = document.getElementById("question-container");
+const choicesContainer = document.getElementById("choices-container");
+const startButton = document.getElementById("start-btn");
+const timerContainer = document.getElementById("timer-container");
+const gameOverContainer = document.getElementById("game-over-container");
+const scoreForm = document.getElementById("score-form");
+const initialsInput = document.getElementById("initials");
+const highestScoreContainer = document.getElementById("highest-score-container");
+
+let currentQuestionIndex = 0;
+let timer;
+let timeRemaining = 60; // Adjust as needed
+let score = 0;
+let highestScore = loadHighestScore();
+
+startButton.addEventListener("click", startQuiz);
+scoreForm.addEventListener("submit", saveScore);
+
+function loadHighestScore() {
+const storedScore = localStorage.getItem("highestScore");
+return storedScore ? JSON.parse(storedScore) : null;
+}
+
+function saveHighestScore(scoreData) {
+localStorage.setItem("highestScore", JSON.stringify(scoreData));
+}
+
+function displayHighestScore(highestScore) {
+if (highestScore) {
+    highestScoreContainer.textContent = `Highest Score: ${highestScore.initials} - ${highestScore.score}`;
+} else {
+    highestScoreContainer.textContent = "No previous highest score recorded.";
+}
+}
+
+function startQuiz() {
+resetQuiz();
+startButton.style.display = "none";
+quizContainer.style.display = "block";
+timerContainer.textContent = "Time: " + timeRemaining;
+startTimer();
+showQuestion();
+}
+
+function resetQuiz() {
+currentQuestionIndex = 0;
+score = 0;
+timeRemaining = 60;
+gameOverContainer.style.display = "none";
+quizContainer.style.display = "block";
+startButton.style.display = "inline-block";
+questionContainer.textContent = "";
+choicesContainer.innerHTML = "";
+timerContainer.textContent = "";
+initialsInput.value = "";
+}
+
+function startTimer() {
+timer = setInterval(function() {
+    timeRemaining--;
     timerContainer.textContent = "Time: " + timeRemaining;
-    startTimer();
-    showQuestion();
-  }
-  
-  function startTimer() {
-    timer = setInterval(function() {
-      timeRemaining--;
-      timerContainer.textContent = "Time: " + timeRemaining;
-  
-      if (timeRemaining <= 0) {
+
+    if (timeRemaining <= 0) {
         clearInterval(timer);
         endQuiz();
-      }
-    }, 1000);
-  }
-  
-  function showQuestion() {
-    const currentQuestion = quizQuestions[currentQuestionIndex];
-    questionContainer.textContent = currentQuestion.question;
-    choicesContainer.innerHTML = "";
-  
-    currentQuestion.choices.forEach(function(choice, index) {
-      const choiceButton = document.createElement("button");
-      choiceButton.textContent = choice;
-      choiceButton.addEventListener("click", function() {
+    }
+}, 1000);
+}
+
+function showQuestion() {
+const currentQuestion = quizQuestions[currentQuestionIndex];
+questionContainer.textContent = currentQuestion.question;
+choicesContainer.innerHTML = "";
+
+currentQuestion.choices.forEach(function(choice, index) {
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", function() {
         checkAnswer(index);
-      });
-      choicesContainer.appendChild(choiceButton);
     });
-  }
-  
-  function checkAnswer(choiceIndex) {
-    const currentQuestion = quizQuestions[currentQuestionIndex];
-  }
-    if (choiceIndex === currentQuestion.correctAnswer) {
-      score++;
-   }
-  
+    choicesContainer.appendChild(choiceButton);
+});
+}
 
+function checkAnswer(choiceIndex) {
+const currentQuestion = quizQuestions[currentQuestionIndex];
 
+if (choiceIndex === currentQuestion.correctAnswer) {
+    score++;
+} else {
+    timeRemaining -= 5; // Deduct 5 seconds for incorrect answer
+    if (timeRemaining < 0) {
+        timeRemaining = 0; // Ensure the timer doesn't go negative
+    }
+}
 
+currentQuestionIndex++;
 
+if (currentQuestionIndex >= quizQuestions.length || timeRemaining <= 0) {
+    endQuiz();
+} else {
+    showQuestion();
+}
+}
+
+function endQuiz() {
+clearInterval(timer);
+quizContainer.style.display = "none";
+gameOverContainer.style.display = "block";
+timerContainer.textContent = "Time: 0";
+
+if (score > (highestScore ? highestScore.score : 0)) {
+    const initials = initialsInput.value.trim();
+    highestScore = { score, initials };
+    saveHighestScore(highestScore);
+    alert("Congratulations! You've achieved a new highest score.");
+}
+}
+
+function saveScore(event) {
+event.preventDefault();
+const initials = initialsInput.value.trim();
+if (initials !== "") {
+    // Implement logic to save the score and initials
+    // For example, you can send an HTTP request to a server
+    // with the score and initials data
+    // Here, we'll just display the score and initials in the console
+    console.log("Score:", score);
+    console.log("Initials:", initials);
+
+    highestScore = { score, initials };
+    saveHighestScore(highestScore);
+    displayHighestScore(highestScore);
+
+    resetQuiz();
+    startButton.style.display = "inline-block";
+}
+}
+
+displayHighestScore(highestScore);
